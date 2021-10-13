628b65a3d0cbfc3ddc1d008fc531aae185c65b9a
Clémence <clemence.lemeilleur@icloud.com>
2021-10-13 08:42:56 +0200

    Test

--- TDs_POO/src/td1/TwoThreadsDemo.java
+++
@@ -1,9 +0,0 @@
-package td1;
-
-public class TwoThreadsDemo { 
-    public static void main (String[] args) {
-        new SimpleThread("Jamaica").start();
-        new SimpleThread("Fiji").start();
-        new SimpleThread("Bora Bora").start(); 
-    }
-}
